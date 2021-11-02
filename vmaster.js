8a9a41b93b99b436ce3cf8488d9f0562bbc229c6
super1114 <super1114dev@gmail.com>
2021-11-02 11:05:43 -0400

    changed manifest

--- vmaster.js
+++ vmaster.js
@@ -11,19 +11,7 @@ function backgroundcheck() {
         document.querySelector(".inputvalue").value = "";
     }
 }
-// document.querySelector(".timeicon").addEventListener("click", () => {
-//     backgroundcheck();
-//     document.querySelector(".inputvalue").setAttribute("type", "time");
-//     document.querySelector(".inputvalue").style.left = "78px";
-//     document.querySelector(".inputvalue").setAttribute("placeholder", "time");
-// })
-// document.querySelector(".membericon").addEventListener("click", () => {
-//     backgroundcheck();
-//     document.querySelector(".inputvalue").setAttribute("type", "number");
-//     document.querySelector(".inputvalue").setAttribute("placeholder", "members");
-//     document.querySelector(".inputvalue").style.left = "78px";
-//     document.querySelector(".inputvalue").style.width = "90px";
-// })
+
 document.querySelector(".minutesicon").addEventListener("click", () => {
     backgroundcheck();
     document.querySelector(".inputvalue").setAttribute("type", "number");
@@ -37,33 +25,16 @@ document.querySelector(".tickicon").addEventListener("click", () => {
         "value": document.querySelector(".inputvalue").value,
         "select": document.querySelector(".inputvalue").getAttribute("placeholder")
     }
-    console.log(obj);
-    // if (obj.value != "") {
-        // if (obj.value < 0) {
-        //     document.querySelector(".inputvalue").style.border = "2px solid red";
-        //     setTimeout(() => {
-        //         document.querySelector(".inputvalue").style.border = "1px solid black";
-        //     }, 2000);
-        // }
-            obj.value = "15";
-            localStorage.setItem("backgroundwork", "yes");
-            document.querySelector(".inputvalue").disabled = true;
-            localStorage.setItem("value", obj.value);
-            localStorage.setItem("area", obj.select);
-            document.querySelector(".hourglassicon").style.visibility = "visible";
-            chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-                var activeTab = tabs[0];
-                chrome.tabs.sendMessage(activeTab.id, { "message": "tickbtnclicked", "object": obj });
-            });
-        
-    // }
-    // else {
-    //     console.log("enter value");
-    //     document.querySelector(".inputvalue").style.border = "2px solid red";
-    //     setTimeout(() => {
-    //         document.querySelector(".inputvalue").style.border = "1px solid black";
-    //     }, 2000);
-    // }
+    obj.value = "15";
+    localStorage.setItem("backgroundwork", "yes");
+    document.querySelector(".inputvalue").disabled = true;
+    localStorage.setItem("value", obj.value);
+    localStorage.setItem("area", obj.select);
+    document.querySelector(".hourglassicon").style.visibility = "visible";
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, { "message": "tickbtnclicked", "object": obj });
+    });
 })
 document.querySelector(".stopicon").addEventListener("click", () => {
 
